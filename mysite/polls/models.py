3780d870f8915bbc7343fd43cf032d02f55413ce
Carson McLean <camclean@ualberta.ca>
2016-10-06 15:39:16 -0600

    Introduces testing of polls

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
+
+import datetime
 
 # Create your models here.
 
@@ -7,6 +10,11 @@ from django.utils.encoding import python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
     def __str__(self):
         return self.question_text
 
--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.utils import timezone
+
+import datetime
+
+from .models import Question
 
 # Create your tests here.
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
